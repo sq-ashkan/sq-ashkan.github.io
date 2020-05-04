820e1b10fc722db955af8398966edea64a682d33
sq-ashkan <samanto@ashkans-MBP.fritz.box>
2020-05-04 18:43:59 +0200

    bug fixed

--- project/vanilla/meal-finder/script.js
+++ project/vanilla/meal-finder/script.js
@@ -1,34 +1,33 @@
 const search = document.getElementById('search'),
-  submit = document.getElementById('submit'),
-  random = document.getElementById('random'),
-  mealsEl = document.getElementById('meals'),
-  resultHeading = document.getElementById('result-heading'),
-  single_mealEl = document.getElementById('single-meal');
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    single_mealEl = document.getElementById('single-meal');
 
 // Search meal and fetch from API
 function searchMeal(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  // Clear single meal
-  single_mealEl.innerHTML = '';
+    // Clear single meal
+    single_mealEl.innerHTML = '';
 
-  // Get search term
-  const term = search.value;
+    // Get search term
+    const term = search.value;
 
-  // Check for empty
-  if (term.trim()) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+    // Check for empty
+    if (term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                resultHeading.innerHTML = `<h2 class="search-text">Search results for '${term}':</h2>`;
 
-        if (data.meals === null) {
-          resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
-        } else {
-          mealsEl.innerHTML = data.meals
-            .map(
-              meal => `
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<p class="search-text">There are no search results. Try again!<p>`;
+                } else {
+                    mealsEl.innerHTML = data.meals
+                        .map(
+                            meal => `
             <div class="meal">
               <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
               <div class="meal-info" data-mealID="${meal.idMeal}">
@@ -36,51 +35,51 @@ function searchMeal(e) {
               </div>
             </div>
           `
-            )
-            .join('');
-        }
-      });
-    // Clear search text
-    search.value = '';
-  } else {
-    alert('Please enter a search term');
-  }
+                        )
+                        .join('');
+                }
+            });
+        // Clear search text
+        search.value = '';
+    } else {
+        alert('Please enter a search term');
+    }
 }
 
 // Fetch meal by ID
 function getMealById(mealID) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-    .then(res => res.json())
-    .then(data => {
-      const meal = data.meals[0];
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
 
-      addMealToDOM(meal);
-    });
+            addMealToDOM(meal);
+        });
 }
 
 // Fetch random meal from API
 function getRandomMeal() {
-  // Clear meals and heading
-  mealsEl.innerHTML = '';
-  resultHeading.innerHTML = '';
+    // Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
 
-  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then(res => res.json())
-    .then(data => {
-      const meal = data.meals[0];
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
 
-      addMealToDOM(meal);
-    });
+            addMealToDOM(meal);
+        });
 }
 
 // Add meal to DOM
 function addMealToDOM(meal) {
-  const ingredients = [];
+    const ingredients = [];
 
-  for (let i = 1; i <= 20; i++) {
-    if (meal[`strIngredient${i}`]) {
-      ingredients.push(
-        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(
+                    `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
       );
     } else {
       break;

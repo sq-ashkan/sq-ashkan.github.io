d2e825265501467decc6d1b6838267bdf895629c
sq-ashkan <samanto@ashkans-MBP.fritz.box>
2020-05-06 15:51:05 +0200

    project add

---
+++ project/react/precache-manifest.6d83ce0111f46d60983f30732e7d5f7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919ccc19dc85900b0a9792f59f5e725a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4548710484e4d992b57",
+    "url": "/static/css/2.9dc5248e.chunk.css"
+  },
+  {
+    "revision": "e3819970bb48bb5ced92",
+    "url": "/static/css/main.9a38c52d.chunk.css"
+  },
+  {
+    "revision": "f4548710484e4d992b57",
+    "url": "/static/js/2.d18560e5.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/static/js/2.d18560e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3819970bb48bb5ced92",
+    "url": "/static/js/main.1aff42ee.chunk.js"
+  },
+  {
+    "revision": "024c6d9d0a197a1cbf3b",
+    "url": "/static/js/runtime-main.970325b7.js"
+  }
+]);

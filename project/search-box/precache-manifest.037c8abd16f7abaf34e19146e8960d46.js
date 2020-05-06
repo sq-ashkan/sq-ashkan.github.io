4862aa09432cd957069a81a7bc9a4d43edde88c0
sq-ashkan <samanto@ashkans-MBP.fritz.box>
2020-05-06 18:25:26 +0200

    update

---
+++ project/search-box/precache-manifest.037c8abd16f7abaf34e19146e8960d46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0797313fcc13441852f614b06d88302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "71918c2c75bec6d0111c",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71918c2c75bec6d0111c",
+    "url": "/static/js/main.08a8c082.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
